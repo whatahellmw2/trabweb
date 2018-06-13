147d3bcb0c519c32bf21bf15fdfecbd439d8e039
VitorMRibeiro <vitormribeiro88@gmail.com>
2018-06-13 15:08:20 -0300

    mandei a decoração dos paragrafos quando o mouse passa do javascript pro css

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-$("p").on({
-	mouseenter: function(){
-		$(this).css( "text-decoration", "underline");
-	},
-	mouseleave: function(){
-		$(this).css("text-decoration","none");
-	}
-	
-});
  $(".img_icone").click(function(){
         $("ul").toggle(500);
         
